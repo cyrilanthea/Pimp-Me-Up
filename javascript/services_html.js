21d619b1bef7bfedebf980ea8dc05950d900ffa4
cyrilanthea <cyrilanthea@gmail.com>
2022-01-16 13:41:19 +0100

    added description for every first image

--- javascript/services_html.js
+++ javascript/services_html.js
@@ -9,7 +9,7 @@ const createServiceCategorie = () => {
       <button class="nxt-btn"><img src="../img/arrow.png" alt=""></button>
       <div class="product-container">
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-50% promo</span> <img src="../img/coiffure2.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-50% promo</span> <a href="../html/detail.html"><img src="../img/coiffure2.jpg" class="product-thumb" alt=""></a> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
@@ -52,7 +52,7 @@ const createServiceCategorie = () => {
       <button class="nxt-btn"><img src="../img/arrow.png" alt=""></button>
       <div class="product-container">
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-50% promo</span> <img src="../img/maquillage2.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-50% promo</span> <a href="../html/detail.html"><img src="../img/maquillage2.jpg" class="product-thumb" alt=""></a> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
