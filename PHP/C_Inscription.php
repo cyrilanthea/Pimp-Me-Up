ddbc2b5fc8a37fa4d046f9d5265212bb07128d72
cyrilanthea <cyrilanthea@gmail.com>
2022-01-16 01:04:56 +0100

    added favicon everywhere + produits par theme

--- PHP/C_Inscription.php
+++ PHP/C_Inscription.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
     <html lang="fr">
         <head>
+            <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
             <meta charset="UTF-8">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <link rel="stylesheet" type="text/css" href="../css/Form.css">
--- PHP/P_Inscription.php
+++ PHP/P_Inscription.php
@@ -5,6 +5,7 @@
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <link rel="stylesheet" type="text/css" href="../CSS/Form.css">
             <title>Inscription</title>
+            <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
         </head>
         <body>
             <?php
