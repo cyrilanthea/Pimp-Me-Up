fb7b84fb53846ee9dd595da31386090f4d36cee9
CYRIL Anthéa <56709604+cyrilanthea@users.noreply.github.com>
2022-01-21 23:38:26 +0100

    Update slider_html.js

--- javascript/slider_html.js
+++ javascript/slider_html.js
@@ -4,15 +4,15 @@ const createSlider = () => {
   slider.innerHTML = `
   <div class="slideshow-container">
     <div class="mySlides fade">
-      <img src="../img/vetements_promo.png" style="width:100%">
+      <img src="img/vetements_promo.png" style="width:100%">
     </div>
 
     <div class="mySlides fade">
-      <img src="../img/bijoux_promo.png"  style="width:100%">
+      <img src="img/bijoux_promo.png"  style="width:100%">
     </div>
 
     <div class="mySlides fade">
-      <img src="../img/coiffure_promo.png"  style="width:100%">
+      <img src="img/coiffure_promo.png"  style="width:100%">
     </div>
   </div>
   <br>
