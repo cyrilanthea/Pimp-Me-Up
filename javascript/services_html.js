7e7a7546e586252931709d28706c672247157e51
CYRIL Anthéa <56709604+cyrilanthea@users.noreply.github.com>
2022-01-22 14:23:32 +0100

    Update services_html.js

--- javascript/services_html.js
+++ javascript/services_html.js
@@ -5,39 +5,39 @@ const createServiceCategorie = () => {
 
   <section class="product">
       <h2 class="product-category">Coiffures</h2>
-      <button class="pre-btn"><img src="img/arrow.png" alt=""></button>
-      <button class="nxt-btn"><img src="img/arrow.png" alt=""></button>
+      <button class="pre-btn"><img src="../img/arrow.png" alt=""></button>
+      <button class="nxt-btn"><img src="../img/arrow.png" alt=""></button>
       <div class="product-container">
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-50% promo</span> <a href="html/detail.html"><img src="img/coiffure2.jpg" class="product-thumb" alt=""></a> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-50% promo</span> <a href="../html/detail.html"><img src="../img/coiffure2.jpg" class="product-thumb" alt=""></a> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="img/coiffure3.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="../img/coiffure3.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="img/coiffure4.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="../img/coiffure4.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="img/coiffure5.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="../img/coiffure5.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card" id="fade-img">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img  src="img/coiffure6.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img  src="../img/coiffure6.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
@@ -52,35 +52,35 @@ const createServiceCategorie = () => {
       <button class="nxt-btn"><img src="img/arrow.png" alt=""></button>
       <div class="product-container">
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-50% promo</span> <a href="html/detail.html"><img src="img/maquillage2.jpg" class="product-thumb" alt=""></a> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-50% promo</span> <a href="../html/detail.html"><img src="../img/maquillage2.jpg" class="product-thumb" alt=""></a> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="img/Maquillage.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="../img/Maquillage.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="img/maquillage1.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="../img/maquillage1.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="img/Maquillage3.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img src="../img/Maquillage3.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
               </div>
           </div>
           <div class="product-card" id="fade-img">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img  src="img/Maquillage.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img  src="../img/Maquillage.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
