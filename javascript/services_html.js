d4725b68f6b7ab8a8be1553f098df6277caa7901
CYRIL Anthéa <56709604+cyrilanthea@users.noreply.github.com>
2022-01-25 11:50:55 +0100

    changed path for arrow, images in maquillage

--- javascript/services_html.js
+++ javascript/services_html.js
@@ -48,8 +48,8 @@ const createServiceCategorie = () => {
 
   <section class="product">
       <h2 class="product-category">Maquillage</h2>
-      <button class="pre-btn"><img src="img/arrow.png" alt=""></button>
-      <button class="nxt-btn"><img src="img/arrow.png" alt=""></button>
+      <button class="pre-btn"><img src="../img/arrow.png" alt=""></button>
+      <button class="nxt-btn"><img src="../img/arrow.png" alt=""></button>
       <div class="product-container">
           <div class="product-card">
               <div class="product-image"> <span class="discount-tag">-50% promo</span> <a href="../html/detail.html"><img src="../img/maquillage2.jpg" class="product-thumb" alt=""></a> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
@@ -80,7 +80,7 @@ const createServiceCategorie = () => {
               </div>
           </div>
           <div class="product-card" id="fade-img">
-              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img  src="../img/Maquillage.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
+              <div class="product-image"> <span class="discount-tag">-20% promo</span> <img  src="../img/maquillage.jpg" class="product-thumb" alt=""> <button class="card-btn">Ajouter aux favoris <i class='bx bx-heart bx-tada'></i><button> </div>
               <div class="product-info">
                   <h2 class="product-brand">Magasin</h2>
                   <p class="product-short-des">Lorem ipsum dolor sit amet..</p> <span class="price">20 €</span> <span class="actual-price">59,99 €</span>
